73a8e05f31739ee3a6804e2f773d7b5bfdbcd83a
BrightTzy <nhtoo862005@gmail.com>
2026-09-03 22:08:09 +0700

    change repo name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web_dev_projet1_test/',
-})
+  base: "/web_dev_project_1/",
+});
